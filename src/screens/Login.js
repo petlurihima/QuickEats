ba1ff584adcd8df1792e42065bd689ffb4195d29
Petluri HimaBindu <himabindupetluri06@gmail.com>
2024-10-01 20:41:40 +0530

    commit

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,72 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  });
+  let navigate=useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/api/loginuser", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        password: credentials.password,
+        email: credentials.email
+      })
+    });
+
+    const json = await response.json();
+    console.log(json);
+    // Handle the response or any error
+    if (!json.success) {
+      alert("Enter valid credentials")
+    }
+    if (json.success) {
+      navigate("/")
+    }
+  }
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  }
   return (
-    <div>Login</div>
+    <>
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              name="email"
+              value={credentials.email}
+              onChange={onChange}
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-success">Submit</button>
+          <Link to="/createuser" className="m-3 btn btn-danger">New User</Link>
+        </form>
+      </div>
+    </>
   )
 }
